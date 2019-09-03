1820fd8be2806f00a7876957eaded7ef4ebcb517
Kristoffer Langstad <54596994+krilangs@users.noreply.github.com>
2019-09-03 15:05:55 +0200

    Working on c

--- Project1/fys4150_prosj1.py
+++ Project1/fys4150_prosj1.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import time
@@ -6,6 +7,8 @@ def f(x):
     """Source term f(x)"""
     return 100.*np.exp(-10.*x)
 
+def analytic(x):
+    return 1 - (1 - np.exp(-10)) * x - np.exp(-10 * x)
 
 def gausselim(n):
     a = np.ones(n-1)*(-1)
@@ -15,7 +18,6 @@ def gausselim(n):
     x = np.linspace(0, 1., n+2)
     h = 1./(n+1)
     
-    #f = 100.*np.exp(-10.*x)*h**2
     b_tilde = np.zeros(n)
     f_tilde = np.zeros(n)
     v = np.zeros(n+2)
@@ -39,42 +41,63 @@ def gausselim(n):
     
     t1 = time.time()
     timer = t1 - t0
-    print("Time used: %s s" %timer )
     
-    return v
+    return v, timer
 
 def special_algorithm(n):
-    x = np.linspace(0, 1., n)
+    x = np.linspace(0, 1., n+2)
     h = 1./(n+1)
     
-    f = 100.*np.exp(-10.*x)*h**2
+    #f = 100.*np.exp(-10.*x)*h**2
+    b_tilde = np.zeros(n)
     f_tilde = np.zeros(n)
-    v = np.zeros(n)
+    v = np.zeros(n+2)
+    f_vec = np.zeros(n)
+    for i in range(0, n):
+        f_vec[i] = f(x[i+1])*h**2
     
-    f_tilde[0] = f[0]
-    
+    b_tilde[0] = b_tilde[-1] = 2.
+    f_tilde[0] = f_vec[0]
+
     # Forward substitution
+    t0 = time.time()
     for i in range(1, n-1):
-        f_tilde[i] = f[i] - (i-1)*f_tilde[i-1]/i
-    
+        b_tilde[i] = (i+1.)/i
+    for i in range(1, n):
+        f_tilde[i] = f_vec[i] + f_tilde[i-1]/b_tilde[i-1]
+
     # Backward substitution
-    v[n-1] = f_tilde[n-1]/b_tilde[n-1]
-    for i in range(n-2, 1):
-        v[i] = (f_tilde[i] + v[i+1])*(i-1)/i
+    v[n] = f_tilde[n-1]/b_tilde[n-1]
+    for i in range(n-1, 0, -1):
+        v[i] = (f_tilde[i-1] + v[i+1])/b_tilde[i-1]
         
-    return v
-        
-def analytic(x):
-    return 1 - (1 - np.exp(-10)) * x - np.exp(-10 * x)
+    t1 = time.time()
+    timer = t1 - t0
+    #print("Time used special: %s s" %timer )
     
+    return v, timer    
 
 if __name__=="__main__":
+#b)
+    """
     N = [10, 100, 1000]
-    
     for n in N:
         plt.figure()
         x = np.linspace(0, 1., n+2)
-        plt.plot(x, gausselim(n), label="Numerical for n=%s" %n)
+        v, time1 = gausselim(n)
+        print("Time used general: %s s" %time1)
+        plt.plot(x, v, label="Numerical for n=%s" %n)
         plt.plot(x, analytic(x), "--r", label="Analytical for n=%s" %n)
         plt.legend()
-    
+    """
+#c) 
+    N = [10, 1e2, 1e3, 1e4, 1e5, 1e6]
+    for n in N:
+        n = int(n)
+        time1 = gausselim(n)[1]
+        time2 = special_algorithm(n)[1]
+        print("n=%.1e" %n)
+        print("Time used general: %.3e s" %time1)
+        print("Time used special: %.3e s" %time2)
+        time_diff = time1 - time2
+        print("Time diff: %.3e" %time_diff)
