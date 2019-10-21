68a08b2451f17ee1486e546268fe28fc1d433737
Kristoffer Langstad <54596994+krilangs@users.noreply.github.com>
2019-10-21 20:28:55 +0200

    Delete weights.h

--- Project3/weights.h
+++
@@ -1,9 +0,0 @@
-#ifndef WEIGHTS_H
-#define WEIGHTS_H
-#include "weights.cpp"
-
-void gauleg(double, double, double *, double *, int);
-void gauss_laguerre(double *, double *, int, double);
-double gammln(double);
-
-#endif // WEIGHTS_H
