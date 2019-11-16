92375fd8f55f832d161bfb38079f99969a8302a8
Kristoffer Langstad <54596994+krilangs@users.noreply.github.com>
2019-11-16 16:33:38 +0100

    Add files via upload

--- Project4/Codes/plots.py
+++ Project4/Codes/plots.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+fonts = {"font.size": 14}
+plt.rcParams.update(fonts)
+
 print("Choose which Task to run by typing the one of the letters (C, D, E, F):")
-print("Task C - Equilibrium plots:")
-print("Task D - probability histogram")
-print("Task E - phase transition plots")
+print("Task C - Equilibrium plots")
+print("Task D - Probability histogram")
+print("Task E - Phase transition plots")
 print("Task F - Critical temperature")
 
-Task = input("Write here: ")
+Task = input("Write here (C, D, E, F): ")
 
 if Task == "C":
     MCcycles = []
@@ -62,7 +65,7 @@ if Task == "C":
             Nconfigs_U2.append(float(vals[3]))
 
     plt.figure()
-    plt.title("Mean energy, $\\langle E \\rangle$, with ordered spin")
+    plt.title("Mean energy, $\\langle E \\rangle$,\n with ordered spin")
     plt.plot(MCcycles, E_mean_O1)
     plt.xlabel("# of Monte Carlo cycles")
     plt.ylabel("$\\langle$ E $\\rangle$")
@@ -71,7 +74,7 @@ if Task == "C":
     plt.savefig("Figures/E_Ordered_1.png")
 
     plt.figure()
-    plt.title("Mean energy, $\\langle E \\rangle$, with ordered spin")
+    plt.title("Mean energy, $\\langle E \\rangle$,\n with ordered spin")
     plt.plot(MCcycles, E_mean_O2, "orange")
     plt.xlabel("# of Monte Carlo cycles")
     plt.ylabel("$\\langle$ E $\\rangle$")
@@ -80,7 +83,7 @@ if Task == "C":
     plt.savefig("Figures/E_Ordered_2_4.png")
 
     plt.figure()
-    plt.title("Mean energy, $\\langle E \\rangle$, with random spin")
+    plt.title("Mean energy, $\\langle E \\rangle$,\n with random spin")
     plt.plot(MCcycles, E_mean_U1)
     plt.plot(MCcycles, E_mean_U2)
     plt.xlabel("# of Monte Carlo cycles")
@@ -90,7 +93,7 @@ if Task == "C":
     plt.savefig("Figures/E_Random.png")
 
     plt.figure()
-    plt.title("Mean absolute Magnetization, $\\langle |M| \\rangle$,"
+    plt.title("Mean absolute Magnetization, $\\langle |M| \\rangle$,\n"
               + " with ordered spin")
     plt.plot(MCcycles, M_mean_O1)
     plt.xlabel("# of Monte Carlo cycles")
@@ -100,7 +103,7 @@ if Task == "C":
     plt.savefig("Figures/M_Ordered_1.png")
 
     plt.figure()
-    plt.title("Mean absolute Magnetization, $\\langle |M| \\rangle$,"
+    plt.title("Mean absolute Magnetization, $\\langle |M| \\rangle$,\n"
               + " with ordered spin")
     plt.plot(MCcycles, M_mean_O2, "orange")
     plt.xlabel("# of Monte Carlo cycles")
@@ -110,8 +113,8 @@ if Task == "C":
     plt.savefig("Figures/M_Ordered_2_4.png")
 
     plt.figure()
-    plt.title("Mean absolute Magnetization, $\\langle |M| \\rangle$,"
-              + "with random spin")
+    plt.title("Mean absolute Magnetization, $\\langle |M| \\rangle$,\n"
+              + " with random spin")
     plt.plot(MCcycles, M_mean_U1)
     plt.plot(MCcycles, M_mean_U2)
     plt.xlabel("# of Monte Carlo cycles")
@@ -121,8 +124,8 @@ if Task == "C":
     plt.savefig("Figures/M_Random.png")
 
     plt.figure()
-    plt.title("# of accepted configurations (normalized) w/ ordered spin\n"
-              + "as function of MC cycles")
+    plt.title("# of accepted configurations (normalized)\n w/ ordered spin"
+              + " as function of MC cycles")
     plt.plot(MCcycles, Nconfigs_O1)
     plt.xlabel("# of Monte Carlo cycles")
     plt.ylabel("Accepted configurations (normalized)")
@@ -131,8 +134,8 @@ if Task == "C":
     plt.savefig("Figures/Accpt_Ordered_1.png")
 
     plt.figure()
-    plt.title("# of accepted configurations (normalized) w/ ordered spin\n"
-              + "as function of MC cycles")
+    plt.title("# of accepted configurations (normalized)\n w/ ordered spin"
+              + " as function of MC cycles")
     plt.plot(MCcycles, Nconfigs_O2, "orange")
     plt.xlabel("# of Monte Carlo cycles")
     plt.ylabel("Accepted configurations (normalized)")
@@ -141,8 +144,8 @@ if Task == "C":
     plt.savefig("Figures/Accpt_Ordered_2_4.png")
 
     plt.figure()
-    plt.title("# of accepted configurations (normalized) w/ random spin\n\
-              as function of MC cycles")
+    plt.title("# of accepted configurations (normalized)\n w/ random spin"
+              + " as function of MC cycles")
     plt.plot(MCcycles, Nconfigs_U1)
     plt.plot(MCcycles, Nconfigs_U2)
     plt.xlabel("# of Monte Carlo cycles")
@@ -153,7 +156,6 @@ if Task == "C":
 
     with open("Nconfig_vs_Temp") as file:
         lines = file.readlines()
-
         T = []
         Nconfigs = []
         for i in range(2, len(lines)):
@@ -174,7 +176,6 @@ if Task == "C":
 
 if Task == "D":
     filenames = ["Probability_1","Probability_24"]
-
     for i in filenames:
         with open(i) as file:
             lines = file.readlines()
@@ -195,7 +196,7 @@ if Task == "D":
             plt.figure()
             props = dict(boxstyle="square", facecolor="wheat", alpha=1)
             if i == "Probability_1":
-                plt.title("Probability distribution, P(E), with L=20 and T=1.0")
+                plt.title("Probability distribution, P(E),\n with L=20 and T=1.0")
                 plt.bar(E, counts, width = 4)
                 plt.xlim(-805,-770)
                 plt.text(0.3*(plt.xlim()[1]-plt.xlim()[0])+plt.xlim()[0] ,plt.ylim()[1]*0.85,
@@ -205,10 +206,10 @@ if Task == "D":
                 plt.tight_layout()
                 plt.savefig("Figures/Prob_1.png")
             else:
-                plt.title("Probability distribution, P(E), with L=20 and T=2.4")
+                plt.title("Probability distribution, P(E),\n with L=20 and T=2.4")
                 plt.bar(E, counts, width = 3)
                 plt.xlim(-705,-305)
-                plt.text(0.05*(plt.xlim()[1]-plt.xlim()[0])+plt.xlim()[0] ,plt.ylim()[1]*0.85,
+                plt.text(0.02*(plt.xlim()[1]-plt.xlim()[0])+plt.xlim()[0] ,plt.ylim()[1]*0.85,
                  "Most probable energy:\n" + str(most_prob_energy), bbox = props)
                 plt.xlabel("Energy")
                 plt.ylabel("Energy counts, P(E)")
@@ -217,149 +218,149 @@ if Task == "D":
     plt.show()
 
 if Task == "E":
-    filename = "Phase_transitions"
-
-    with open(filename) as file:
+    with open("Phase_transitions") as file:
         lines = file.readlines()
-        T = []
-        E = []
-        M = []
-        Cv = []
-        Xi = []
-        split = []
-        for j in range(2, len(lines)):
-            line = lines[j]
-            pieces = line.split()
-            split.append(len(line))
-            T.append(float(pieces[0]))
-            E.append(float(pieces[1]))
-            M.append(float(pieces[2]))
-            Cv.append(float(pieces[3]))
-            Xi.append(float(pieces[4]))
+    T = []
+    E = []
+    M = []
+    Cv = []
+    Xi = []
+    split = []
+    for j in range(2, len(lines)):
+        line = lines[j]
+        vals = line.split()
+        split.append(len(line))
+        T.append(float(vals[0]))
+        E.append(float(vals[1]))
+        M.append(float(vals[2]))
+        Cv.append(float(vals[3]))
+        Xi.append(float(vals[4]))
 
-        t_split = len(split)/4.
-        t1 = int(t_split)
-        t2 = int(2*t_split)
-        t3 = int(3*t_split)
-        t4 = int(4*t_split)
-        temps = T[:t1]
+    t_split = len(split)/4.
+    t1 = int(t_split)
+    t2 = int(2*t_split)
+    t3 = int(3*t_split)
+    t4 = int(4*t_split)
+    temps = T[:t1]
 
-        plt.figure()
-        plt.title("Phase transition plot for $\\langle E(T) \\rangle$:\n"
-                  + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
-        plt.plot(temps, E[:t1], label="40x40")
-        plt.plot(temps, E[t1:t2], label="60x60")
-        plt.plot(temps, E[t2:t3], label="80x80")
-        plt.plot(temps, E[t3:t4], label="100x100")
-        plt.xlabel("Temperatures [kT/J]")
-        plt.ylabel("$\\langle$ E(T) $\\rangle$")
-        plt.tight_layout()
-        plt.legend()
-        plt.savefig("Figures/Phase_E(T).png")
+    plt.figure()
+    plt.title("Phase transition plot for $\\langle E(T) \\rangle$:\n"
+              + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
+    plt.plot(temps, E[:t1], label="40x40")
+    plt.plot(temps, E[t1:t2], label="60x60")
+    plt.plot(temps, E[t2:t3], label="80x80")
+    plt.plot(temps, E[t3:t4], label="100x100")
+    plt.xlabel("Temperatures [kT/J]")
+    plt.ylabel("$\\langle$ E(T) $\\rangle$")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig("Figures/Phase_E(T).png")
 
-        plt.figure()
-        plt.title("Phase transition plot for $\\langle |M(T)| \\rangle$:\n"
-                  + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
-        plt.plot(temps, M[:t1], label="40x40")
-        plt.plot(temps, M[t1:t2], label="60x60")
-        plt.plot(temps, M[t2:t3], label="80x80")
-        plt.plot(temps, M[t3:t4], label="100x100")
-        plt.xlabel("Temperatures [kT/J]")
-        plt.ylabel("$\\langle |M(T)| \\rangle$")
-        plt.tight_layout()
-        plt.legend()
-        plt.savefig("Figures/Phase_M(T).png")
+    plt.figure()
+    plt.title("Phase transition plot for $\\langle |M(T)| \\rangle$:\n"
+              + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
+    plt.plot(temps, M[:t1], label="40x40")
+    plt.plot(temps, M[t1:t2], label="60x60")
+    plt.plot(temps, M[t2:t3], label="80x80")
+    plt.plot(temps, M[t3:t4], label="100x100")
+    plt.xlabel("Temperatures [kT/J]")
+    plt.ylabel("$\\langle |M(T)| \\rangle$")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig("Figures/Phase_M(T).png")
 
-        plt.figure()
-        plt.title("Phase transition plot for $C_v$:\n"
-                  + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
-        plt.plot(temps, Cv[:t1], label="40x40")
-        plt.plot(temps, Cv[t1:t2], label="60x60")
-        plt.plot(temps, Cv[t2:t3], label="80x80")
-        plt.plot(temps, Cv[t3:t4], label="100x100")
-        plt.xlabel("Temperatures [kT/J]")
-        plt.ylabel("Specific heat $C_v$")
-        plt.tight_layout()
-        plt.legend()
-        plt.savefig("Figures/Phase_Cv(T).png")
+    plt.figure()
+    plt.title("Phase transition plot for $C_v$:\n"
+              + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
+    plt.plot(temps, Cv[:t1], label="40x40")
+    plt.plot(temps, Cv[t1:t2], label="60x60")
+    plt.plot(temps, Cv[t2:t3], label="80x80")
+    plt.plot(temps, Cv[t3:t4], label="100x100")
+    plt.xlabel("Temperatures [kT/J]")
+    plt.ylabel("Specific heat $C_v$")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig("Figures/Phase_Cv(T).png")
 
-        plt.figure()
-        plt.title("Phase transition plot for $\\chi$:\n"
-                  + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
-        plt.plot(temps, Xi[:t1], label="40x40")
-        plt.plot(temps, Xi[t1:t2], label="60x60")
-        plt.plot(temps, Xi[t2:t3], label="80x80")
-        plt.plot(temps, Xi[t3:t4], label="100x100")
-        plt.xlabel("Temperatures [kT/J]")
-        plt.ylabel("Susceptibility $\\chi$")
-        plt.tight_layout()
-        plt.legend()
-        plt.savefig("Figures/Phase_Xi(T).png")
-        plt.show()
+    plt.figure()
+    plt.title("Phase transition plot for $\\chi$:\n"
+              + "L=[40,60,80,100], $\\Delta$t=0.002, MC=5e5")
+    plt.plot(temps, Xi[:t1], label="40x40")
+    plt.plot(temps, Xi[t1:t2], label="60x60")
+    plt.plot(temps, Xi[t2:t3], label="80x80")
+    plt.plot(temps, Xi[t3:t4], label="100x100")
+    plt.xlabel("Temperatures [kT/J]")
+    plt.ylabel("Susceptibility $\\chi$")
+    plt.tight_layout()
+    plt.legend()
+    plt.savefig("Figures/Phase_Xi(T).png")
+    plt.show()
 
 if Task == "F":
     with open("Phase_transitions") as file:
         lines = file.readlines()
-    temps = []
-    energylist = []
-    maglist = []
-    Cvlist = []
-    Suscplist = []
-    indeks = 0
-    for i in range(2, len(lines)):
-        pieces = lines[i].split()
-        temps.append(float(pieces[0]))
-        energylist.append(float(pieces[1]))
-        maglist.append(float(pieces[2]))
-        Cvlist.append(float(pieces[3]))
-        Suscplist.append(float(pieces[4]))
+    T = []
+    E = []
+    M = []
+    Cv = []
+    Xi = []
+    split = []
+    for j in range(2, len(lines)):
+        line = lines[j]
+        pieces = line.split()
+        split.append(len(line))
+        T.append(float(pieces[0]))
+        E.append(float(pieces[1]))
+        M.append(float(pieces[2]))
+        Cv.append(float(pieces[3]))
+        Xi.append(float(pieces[4]))
 
-    firstTemp = temps[0]
-    for i in range(2,len(temps)):
-        if temps[i] == firstTemp:
-            temps = temps[0:i]
-            break
-
+    t_split = len(split)/4.
+    t1 = int(t_split)
+    t2 = int(2*t_split)
+    t3 = int(3*t_split)
+    t4 = int(4*t_split)
+    temps = T[:t1]
     TCCv = []
     TCX = []
-    for i in range(int(len(energylist)/len(temps))):
-        max_temp = 0
-        sublistCv = Cvlist[i*len(temps):len(temps)*(i+1)]
-        sublistSuscp = Suscplist[i*len(temps):len(temps)*(i+1)]
-        maxCv = max(sublistCv)
-        maxSuscp = max(sublistSuscp)
-        TCCv.append(temps[sublistCv.index(maxCv)])
-        TCX.append(temps[sublistSuscp.index(maxSuscp)])
-        print("Tc for Cv =",temps[sublistCv.index(maxCv)])
-        print("Tc for X =",temps[sublistSuscp.index(maxSuscp)])
+
+    for i in range(int(len(E)/len(temps))):
+        listCv = Cv[i*len(temps):len(temps)*(i+1)]
+        listXi = Xi[i*len(temps):len(temps)*(i+1)]
+        maxCv = max(listCv)
+        maxXi = max(listXi)
+        TCCv.append(temps[listCv.index(maxCv)])
+        TCX.append(temps[listXi.index(maxXi)])
+        print("Tc for Cv =",temps[listCv.index(maxCv)])
+        print("Tc for Xi =",temps[listXi.index(maxXi)])
 
     #Performing a linear regression to find critical temp in thermodyn. limit
     TCCv = np.array(TCCv)
     TCX = np.array(TCX)
-    Llist = np.array([40,60,80,100])
-    Llist = 1.0/Llist
+    Llist = 1.0/np.array([40,60,80,100])
+    TC = 2./(np.log(1+np.sqrt(2)))
 
     linreg1 = np.polyfit(Llist,TCCv,1)
     linreg2 = np.polyfit(Llist,TCX,1)
 
     plt.figure()
-    plt.title("Specific heat $C_V$")
-    plt.xlabel("$\\frac{1}{L}$")
-    plt.ylabel("$T_C$ [kT/J]")
-    plt.plot(Llist,TCCv,"o")
-    plt.plot(Llist,np.polyval(linreg1,Llist))
-    plt.legend(["$T_C$(L) from simulations","$T_C(L)$ = a$\\cdot$ $\\frac{1}{L}$ + $T_C(L = \infty)$ $\\to$ %g$\\cdot$x + %g" % (linreg1[0],linreg1[1])])
+    plt.title("Phase transitions for specific heat with\n $T_C$ for"
+              + " analytic value and L=[40,60,80,100]")
+    plt.xlabel("T [kT/J]")
+    plt.ylabel("Specific heat $\langle$$C_v$$\\rangle$ [$J^2/kT^2$]")
+    plt.plot(temps[:t1], Cv[:t1], label="_nolegend_")
+    plt.plot(temps[:t1], Cv[t1:t2], label="_nolegend_")
+    plt.plot(temps[:t1], Cv[t2:t3], label="_nolegend_")
+    plt.plot(temps[:t1], Cv[t3:t4], label="_nolegend_")
+    for i in range(int(len(E)/len(temps))):
+        plt.plot([TCCv[i], TCCv[i]], [min(Cv[i*len(temps):len(temps)*(i+1)]),
+                 max(Cv[i*len(temps):len(temps)*(i+1)])], "--")
+    plt.plot([TC, TC], [min(Cv), max(Cv)], "black")
+    plt.legend(["TC: L = 40","TC: L = 60","TC: L = 80","TC: L = 100", "Analytic TC"])
+    plt.tight_layout()
+    plt.savefig("Figures/CV_TC.png")
 
-    plt.figure()
-    plt.title("Susceptibility $\\chi$")
-    plt.xlabel("$\\frac{1}{L}$")
-    plt.ylabel("$T_C$ [kT/J]")
-    plt.plot(Llist,TCX,"o")
-    plt.plot(Llist,np.polyval(linreg2,Llist))
-    plt.legend(["$T_C$(L) from simulations","$T_C(L)$ = a$\\cdot$ $\\frac{1}{L}$ + $T_C(L = \infty)$ $\\to$ %g$\\cdot$x + %g" % (linreg2[0],linreg2[1])])
-
-    exact = 2./(np.log(1+np.sqrt(2)))
-    print("\n")
-    print("The estimated Critical Temperature from our simulations is Tc = %g " % (0.5*(linreg1[1]+linreg2[1])))
-    print("Exact critical temperature is around %.4f" %exact)
+    print("The estimated Critical Temperature from our simulations is Tc = %g "
+          %(0.5*(linreg1[1]+linreg2[1])))
+    print("Exact critical temperature is around %.4f" %TC)
     plt.show()
